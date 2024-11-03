41958e450f4db9984b57fe6e0e4b13d92fb330c4
molokov_ia <molokov_ia@taximaxim.ru>
2024-11-03 16:54:31 +0300

    Сделал через события

--- L7Delivery/L7Delivery.cs
+++ L7Delivery/L7Delivery.cs
@@ -11,29 +11,11 @@ public class L7Delivery : ILesson
         var goldSecret  = new GoldSecret();
         var magicColor = new MagicColor();
         var simpleFirm = new SimpleFirm();
-
-
+        
         goldSecret.MakeOrderRequest("Адрес1", "Биба", 80);
         magicColor.MakeOrderRequest("Адрес2", "Стас Барецкий", 100, Colors.Red, Colors.Yellow);
         goldSecret.MakeOrderRequest("Адрес3", "Боба", 90);
         simpleFirm.MakeOrderRequest("Адрес4", "Чича", 50);
-        
-        /*OrderManager.CreateNewOrder(
-            goldSecret,
-            goldSecret.MakeOrderRequest("Адрес1", "Биба", 80));
-        
-        OrderManager.CreateNewOrder(
-            magicColor,
-            magicColor.MakeOrderRequest("Адрес2", "Стас Барецкий", 100, Colors.Red, Colors.Yellow));
-        
-        OrderManager.CreateNewOrder(
-            goldSecret,
-            goldSecret.MakeOrderRequest("Адрес3", "Боба", 90));
-        
-        OrderManager.CreateNewOrder(
-            simpleFirm,
-            simpleFirm.MakeOrderRequest("Адрес4", "Чича", 50));
-            */
         
         Console.WriteLine("Текущие заказы");
         OrderManager.ShowOrderList();
